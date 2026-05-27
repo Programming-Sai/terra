11144d34df72d543d32bc453120b1323087ac47c
Programming-Sai <saiahprog6@gmail.com>
2026-05-27 03:31:16 +0000

    fix: add force-dynamic to admin settings page to prevent build-time DB connection errors

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,5 +1,6 @@
 import { AdminSettingsView } from "@/components/admin/admin-settings-view";
 import { getSiteSettings } from "@/lib/site-settings";
+export const dynamic = "force-dynamic";
 
 export default async function AdminSettingsPage() {
   const initialSettings = await getSiteSettings();
